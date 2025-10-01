b078f21eebcc14d0bf9dba11619fff09dd900a34
Poryoculus <rodrigopachecojor@gmail.com>
2025-10-01 16:55:26 -0300

    ssl for render

--- database/index.js
+++ database/index.js
@@ -34,6 +34,9 @@ module.exports = {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+   ssl: {
+      rejectUnauthorized: false,
+    },
   })
   module.exports = pool
 }
