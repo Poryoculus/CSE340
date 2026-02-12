c55a2dba4adc88410d4d7812f0332f6c19e33399
Poryoculus <rodrigopachecojor@gmail.com>
2026-02-12 19:02:41 -0300

    Assignment 4 fix

--- database/changetype.sql
+++ database/changetype.sql
@@ -1,3 +1,9 @@
+
+UPDATE public.account
+SET account_type = 'Employee'
+WHERE account_email = 'happy@340.edu';
+
+
 UPDATE public.account
 SET account_type = 'Admin'
-WHERE account_email = 'basic@340.com';
+WHERE account_email = 'manager@340.edu';
--- database/cse340db.sql
+++ database/cse340db.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340rp;
+    OWNER TO cse340db_gwrm;
 
 
 -- Table structure for table 'classification'
